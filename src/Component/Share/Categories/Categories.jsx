a8f111cbc9086678d1043b7f52603cf86a55e92b
Tanvir225 <tanvirrehman200@gmail.com>
2024-05-07 18:38:20 +0600

    categories design changed

--- src/Component/Share/Categories/Categories.jsx
+++ src/Component/Share/Categories/Categories.jsx
@@ -1,7 +1,7 @@
 
 const Categories = ({text,src}) => {
     return (
-        <div className="flex flex-col justify-center items-center text-center space-y-3">
+        <div className="flex flex-col justify-center items-center text-center space-y-3 py-2">
             <img src="" alt="categories"  className="rounded-full w-[100px] h-[100px] lg:w-[125px] lg:h-[125px] bg-slate-400"/>
             <h3 className="font-semibold text-neutral-600 py-2 text-base md:text-xl border-b-4 border-sky-300">{text}</h3>
         </div>
