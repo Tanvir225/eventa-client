5486c8e1d0cac71582bb9ed3ebc540877a9a6ba2
Tanvir225 <tanvirrehman200@gmail.com>
2024-05-11 18:32:55 +0600

    some change added in select file

--- src/Component/Home/Select.jsx
+++ src/Component/Home/Select.jsx
@@ -3,10 +3,10 @@ import { useState } from "react";
 /* eslint-disable react/prop-types */
 const Select = ({ options, title }) => {
   const [value, setValue] = useState("");
-
+  console.log(value);
   return (
     <div className="w-full px-5">
-      <input type="text" list="select" placeholder={`${title}`} className="w-full lg:w-[350px] input input-info" />
+      <input type="text" list="select" onSelect={(e)=>setValue(e.target.value)} placeholder={`${title}`} className="w-full lg:w-[350px] input input-info" />
       <datalist id="select" className="">
         {options &&
           options.length > 0 &&
